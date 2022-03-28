c8d75b56fddfe68888db84562544f911117a22f0
chaudhuree <chaudhuree00@gmail.com>
2022-03-29 03:06:50 +0600

    docs: action as variable

--- src/App.js
+++ src/App.js
@@ -1,39 +1,33 @@
 import React from "react";
 import { createStore } from "redux";
+// import actions
+import { DECREASE, INCREASE } from "./actions";
 // items
 import cartItems from "./cart-items";
 import CartContainer from "./components/CartContainer";
 // components
 import Navbar from "./components/Navbar";
+
 // redux stuff
-const initialState = { count: 14 ,name:'chaudhuree'};
+const initialState = { count: 14, name: "chaudhuree" };
 
 function reducer(state, action) {
-  if (action.type === "DECREASE") {
-    return {...state, count: state.count - 1};
- 
+  if (action.type === DECREASE) {
+    return { ...state, count: state.count - 1 };
   }
-  if (action.type === "INCREASE") {
-return{...state, count: state.count + 1,name:'sOhan chaudhuree'};
+  if (action.type === INCREASE) {
+    return { ...state, count: state.count + 1, name: "sOhan chaudhuree" };
   }
-  if(action.type === "RESET"){
-    return {...state, count: 0};
-  }
-  if(action.type === "CHANGE_NAME"){
-    return {...state, name: action.payload};
-  }
-  
+
+
   return state;
 }
 
 const store = createStore(reducer, initialState);
 
 // store.getState() returns the current state of the store
-store.dispatch({ type: "DECREASE" });
-store.dispatch({ type: "RESET" });
-store.dispatch({ type: "INCREASE" });
-store.dispatch({ type: "CHANGE_NAME", payload: "sOhan" });
-console.log(store.getState().name);
+store.dispatch({ type: DECREASE });
+store.dispatch({ type: INCREASE });
 
 function App() {
   // cart setup
