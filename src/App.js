dcc68cb261230c40540c185e7093d5a1a9ef3bd6
chaudhuree <chaudhuree00@gmail.com>
2022-03-29 00:58:35 +0600

    docs: added code for store initialization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
-// components
-import Navbar from "./components/Navbar";
-import CartContainer from "./components/CartContainer";
+import { createStore } from "redux";
 // items
 import cartItems from "./cart-items";
+import CartContainer from "./components/CartContainer";
+// components
+import Navbar from "./components/Navbar";
 // redux stuff
+const initialState = { count: 0 };
+function reducer(state, action) {
+  console.log("reducer");
+}
+
+const store = createStore(reducer, initialState);
 
 function App() {
   // cart setup
