c8ea0f4109ebe7a4345ab78779af19c75ff5d417
chaudhuree <chaudhuree00@gmail.com>
2022-03-30 14:47:52 +0600

    docs: decrease function added

--- src/reducer.js
+++ src/reducer.js
@@ -26,8 +26,23 @@ export default function reducer(state, action) {
     };
   }
   if (action.type === DECREASE) {
-    return {};
-  }
+    let tempCart=[]
+        if(action.payload.amount===1){  
+          tempCart=state.cart.filter((item)=>item.id!==action.payload.id)      
+        }
+        else{
+            tempCart=state.cart.map((item)=>{
+                if(item.id===action.payload.id){
+                    item={...item,amount:item.amount-1}
+                }
+                return item
+            })
+        }
+        return {
+            ...state,
+            cart:tempCart
+        }
+    }
 
   return state;
 }
