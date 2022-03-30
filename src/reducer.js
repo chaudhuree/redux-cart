c9a8a8b494e1213859292eefcd38b0945aa47432
chaudhuree <chaudhuree00@gmail.com>
2022-03-30 14:37:51 +0600

    docs:increase functionality added

--- src/reducer.js
+++ src/reducer.js
@@ -1,25 +1,33 @@
 import { CLEAR_CART, DECREASE, INCREASE, REMOVE } from "./components/action";
 export default function reducer(state, action) {
-  if(action.type === CLEAR_CART){
+  if (action.type === CLEAR_CART) {
     return {
       ...state,
       cart: [],
-    }
+    };
   }
-  if(action.type === REMOVE){
+  if (action.type === REMOVE) {
     return {
       ...state,
-      cart: state.cart.filter(item => item.id !== action.payload.id)
-    }
+      cart: state.cart.filter((item) => item.id !== action.payload.id),
+    };
   }
-  if(action.type === INCREASE){
-    console.log('increase');
-  }
-  if(action.type === DECREASE){
-    console.log('decrease');
-  }
+  if (action.type === INCREASE) {
+    let tempCart = state.cart.map((item) => {
+      if (item.id === action.payload.id) {
+        item = { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
 
-
+    return {
+      ...state,
+      cart: tempCart,
+    };
+  }
+  if (action.type === DECREASE) {
+    return {};
+  }
 
   return state;
 }
