8c893d29544dbf395d82d10744f68d24b0e63ccc
chaudhuree <chaudhuree00@gmail.com>
2022-03-30 13:38:56 +0600

    docs: add  function to cart for removeitem

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,21 @@
-import { CLEAR_CART } from "./components/action";
+import { CLEAR_CART, DECREASE, INCREASE, REMOVE } from "./components/action";
 export default function reducer(state, action) {
   if(action.type === CLEAR_CART){
     return {
       ...state,
       cart: [],
-      total: 0,
-      amount: 0
     }
   }
+  if(action.type === REMOVE){
+    console.log('remove');
+  }
+  if(action.type === INCREASE){
+    console.log('increase');
+  }
+  if(action.type === DECREASE){
+    console.log('decrease');
+  }
+
 
 
   return state;
